224ce1c9bcf72db5c214afe144ed7ea98e0b6e59
Cannarron <cannarron@gmail.com>
2021-08-16 14:45:16 +0100

    (chore): Set up database

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const express =  require('express');
+const connectWithMongo = require('./config/database')
+
+require('dotenv').config();
+connectWithMongo()
+
+const app = express();
+
+module.exports = app;
