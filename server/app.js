9bac13a0ed5f659cd01b2134e38c749b1179fa4b
Cannarron <cannarron@gmail.com>
2021-08-31 14:24:23 +0100

    (chore): Rearrange files

--- server/app.js
+++ server/app.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.status(200).send('Now connected to the Meal Plan API');
 });
+app.post('/api/v1/info', validateInput, meal.generatePlan);
+
 
 app.listen(port, () => {
   console.log(`Web server now listening on port ${port}`);
